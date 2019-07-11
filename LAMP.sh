fef94cb347dd9543aa2567131415a49e6c04ab7d
hawk6242 <clopez624242@gmail.com>
2019-07-10 23:14:14 -0700

    Update LAMP.sh

--- LAMP.sh
+++ LAMP.sh
@@ -8,7 +8,7 @@ chmod +x *.sh
 ./RemoveBloat.sh
 ./Updates.sh
 
-sudo apt install apache2 apache2-utils php7.2 php7.2-mysql libapache2-mod-php7.2 php7.2-cli php7.2-cgi php7.2-gd htop vim -y
+sudo apt install apache2 apache2-utils php7.2 php7.2-mysql libapache2-mod-php7.2 php7.2-cli php7.2-cgi php7.2-gd htop vim mysql-client mysql-server -y
 sudo systemctl enable apache2
 cd /var/www/html
 sudo touch info.php
@@ -18,14 +18,14 @@ cd /tmp
 mkdir TEMP
 cd TEMP
 touch SecureInstall.sql
-echo "UPDATE mysql.user SET authentication_string=PASSWORD('My$ql2019') WHERE User='root';
+echo "UPDATE mysql.user SET authentication_string=PASSWORD('PASSWORD') WHERE User='root';
 DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';
 FLUSH PRIVILEGES;
 create database WP_database;
-create user 'alice'@'%' identified by 'M@dhat3r2019';
+create user 'USER'@'%' identified by 'PASSWORD';
 GRANT ALL PRIVILEGES ON *.* TO 'alice'@'%' WITH GRANT OPTION;
 SHOW GRANTS FOR alice;
 FLUSH PRIVILEGES;" > SecureInstall.sql
